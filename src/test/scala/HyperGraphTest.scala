2269e450104424e74550526263468bdf876c4367
Gert Vesterberg <gert.vesterberg@gmail.com>
2019-04-05 16:14:33 +0300

    Testdata + loading data

---
+++ src/test/scala/HyperGraphTest.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ArrayBuffer
+
+object HyperGraphTest {
+
+  def main(args : Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Hypergraph greedy").setMaster("local")
+    val sc = new SparkContext(conf)
+    val edgesRDD = sc.textFile(getClass.getResource("/testdata1.txt").getPath).map(_.split(" ").map(_.toInt).toSet)
+
+    for (v <- getEdges(edgesRDD)) {
+      println(v.getNodes.toString())
+    }
+  }
+
+  def getEdges(vertices: RDD[Set[Int]]): Array[HyperEdge] = {
+    vertices.collect.map(f => {
+      new HyperEdge(f.map(new HyperVertex(_)))
+    })
+  }
+}
