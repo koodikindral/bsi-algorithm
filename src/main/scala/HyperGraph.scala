64ac58712907b3f885152020d752b9b0f33171bb
Gert Vesterberg <gert.vesterberg@gmail.com>
2019-04-08 15:39:28 +0300

    Load data from file + small fixes

--- src/main/scala/HyperGraph.scala
+++ src/main/scala/HyperGraph.scala
@@ -1,27 +1,25 @@
 class HyperGraph(private[this] val nodes: Array[HyperVertex], private[this] val edges: Array[HyperEdge]) {
 
-  this.calcSupp
-  this.calcDSupp
+  this.calcSupp()
+  this.calcDSupp()
 
   def getNodes : Array[HyperVertex] = nodes
 
   def getEdges : Array[HyperEdge] = edges
 
-  def getSupp(a: Array[HyperVertex]): Int = {
-    edges.map(k => k.getNodes.intersect(a)).filter(_.nonEmpty).size
-  }
+  def getSupp(a: Array[HyperVertex]): Int = edges.map(k => k.getNodes.intersect(a)).count(_.nonEmpty)
 
-  def calcSupp = {
-    nodes.map(f => {
+  def calcSupp(): Unit = {
+    nodes.foreach(f => {
       f.supp = getSupp(Array(f))
     })
   }
 
-  def calcDSupp() = {
+  def calcDSupp(): Unit = {
     val nn = nodes.sortWith(_.supp < _.supp)
     var n1 = nn.take(0)
 
-    nodes.sortWith(_.supp < _.supp).map(f => {
+    nodes.sortWith(_.supp < _.supp).foreach(f => {
       n1 ++= n1.union(Array(f))
       f.dSupp = getSupp(n1)
     })
@@ -32,7 +30,7 @@ class HyperGraph(private[this] val nodes: Array[HyperVertex], private[this] val 
   }
 
   def getMaxClique: List[HyperVertex] = {
-    nodes.toList.sortWith(_.supp < _.supp).filter( _.dSupp < getCardinality)
+    nodes.toList.filter(_.dSupp < getCardinality).sortWith(_.supp < _.supp)
   }
 
   def getToExplore: List[HyperVertex] = {
--- src/main/scala/HyperGraphProgram.scala
+++ src/main/scala/HyperGraphProgram.scala
@@ -4,12 +4,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 object HyperGraphProgram {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Hypergraph greedy").setMaster("local")
+    val conf = new SparkConf().setAppName("Hypergraph greedy").setMaster("local[1]")
     val sc = new SparkContext(conf)
-    val edgesRDD = sc.textFile(getClass.getResource("/testdata1.txt").getPath).map(_.split(" ").map(_.toInt).toSet)
+    val edgesRDD = sc.textFile(getClass.getResource("/testdata1.txt").getPath)
+      .map(_.split(" ")
+        .map(_.toInt).toSet)
 
     val vertices: Array[HyperVertex] = getVertices(edgesRDD)
-    val edges: Array[HyperEdge] = getEdges(edgesRDD)
+    val edges: Array[HyperEdge] = getEdges(edgesRDD, vertices)
+
+
     val hyperGraph: HyperGraph = createHyperGraph(vertices, edges)
 
     println("ITERATION #1")
@@ -24,23 +28,25 @@ object HyperGraphProgram {
     println(hyperGraph.getToExplore.map(_.getValue))
   }
 
-  def printVertices(graph: HyperGraph) = {
+  def printVertices(graph: HyperGraph): Unit = {
     println("HyperVertices:")
-    graph.getNodes.map(f => {
+    graph.getNodes.foreach(f => {
       println(f)
     })
   }
 
-  def printEdges(graph: HyperGraph) = {
+  def printEdges(graph: HyperGraph): Unit = {
     println("HyperEdges:")
-    graph.getEdges.map(f => {
+    graph.getEdges.foreach(f => {
       println(f.getNodes)
     })
   }
 
-  def getEdges(edges: RDD[Set[Int]]): Array[HyperEdge] = {
+  def getEdges(edges: RDD[Set[Int]], vertices: Array[HyperVertex]): Array[HyperEdge] = {
+    val vertexMap = vertices.groupBy(_.getValue)
+
     edges.collect.map(f => {
-      new HyperEdge(f.map(new HyperVertex(_)))
+      new HyperEdge(f.flatMap(vertexMap(_)))
     })
   }
 
@@ -50,29 +56,7 @@ object HyperGraphProgram {
     })
   }
 
-  /*
-  TODO: Create hypergraph from input file
-   */
   def createHyperGraph(vertices: Array[HyperVertex], edges: Array[HyperEdge]): HyperGraph = {
-    val n1 = new HyperVertex(1)
-    val n2 = new HyperVertex(2)
-    val n3 = new HyperVertex(3)
-    val n4 = new HyperVertex(4)
-    val n5 = new HyperVertex(5)
-    val n6 = new HyperVertex(6)
-    val n7 = new HyperVertex(7)
-    val n8 = new HyperVertex(8)
-
-    val e1 = new HyperEdge(Array(n1, n2))
-    val e2 = new HyperEdge(Array(n2, n3, n7))
-    val e3 = new HyperEdge(Array(n3, n4, n5))
-    val e4 = new HyperEdge(Array(n4, n6))
-    val e5 = new HyperEdge(Array(n6, n7, n8))
-    val e6 = new HyperEdge(Array(n7))
-
-    //new HyperGraph(vertices, edges)
-    new HyperGraph(Array(n1, n2, n3, n4, n5, n6, n7, n8), Array(e1, e2, e3, e4, e5, e6))
+    new HyperGraph(vertices, edges)
   }
-
-
 }
